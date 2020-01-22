a802f75d4c82a48dc17d64b7ce6b092fa512a674
David Francis <davidjayfrancis@gmail.com>
2020-01-21 21:03:50 -0800

    added routes and router

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require("express");
-const postsRoutes = require("./posts/postsRoutes.js");
+const postsRoutes = require("./routes/postsRoutes.js");
+const commentsRoutes = require("./routes/commentsRoutes.js");
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/posts", postsRoutes);
+app.use("/api/posts/:id/comments", commentsRoutes);
+
+app.listen(4000, () => console.log("listening on port 4000"));
